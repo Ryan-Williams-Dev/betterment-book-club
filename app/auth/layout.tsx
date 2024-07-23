343130a551a4b7fefc9b08b28601813a4619b164
Ryan Williams <r.d.williams.ca@gmail.com>
2024-07-22 19:11:21 -0700

    Implemented form for sign up, currently just going to the console, will make sign in page next almost identical

---
+++ app/auth/layout.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import { ReactNode } from "react";
+
+export default function AuthLayout({ children }: { children: ReactNode }) {
+  return (
+    <section>
+      <div className="absolute w-screen h-screen top md:top-20 md:-left-44">
+        <Image
+          className="-z-10 object-cover md:object-contain"
+          src={"/blob.svg"}
+          fill
+          alt="blog image"
+        />
+      </div>
+
+      {children}
+    </section>
+  );
+}
--- app/auth/sign-up/page.tsx
+++ app/auth/sign-up/page.tsx
@@ -1,4 +1,10 @@
-import { TypographyH1, TypographyH3 } from "@/components/typography";
+"use client";
+
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import { TypographyH1 } from "@/components/typography";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -8,27 +14,153 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+
 import { cn } from "@/lib/utils";
-import React from "react";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z
+  .object({
+    name: z
+      .string()
+      .min(3, { message: "Name Must be at least 3 characters." })
+      .max(30, { message: "Name must be less than 30 characters" })
+      .optional(),
+    username: z
+      .string()
+      .min(3, { message: "Username Must be at least 3 characters." })
+      .max(20, { message: "Username must be less than 20 characters" }),
+    email: z.string().email({ message: "Please eneter a valid email address" }),
+    password: z.string(),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  });
 
 const SignUpPage = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      username: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
   return (
-    <div className="min-h-screen w-screen flex flex-col md:flex-row justify-evenly items-center p-4 md:p-16">
-      <TypographyH3>Sign up Now and explore all the benefits</TypographyH3>
-      <Card className={cn("max-w-2xl")}>
+    <div className="min-h-screen max-w-7xl mx-auto gap-4 flex flex-col md:flex-row justify-center md:justify-evenly md:items-center p-4 md:p-16">
+      <div>
+        <TypographyH1>Grow together with Betterment Book Club</TypographyH1>
+      </div>
+
+      <Card>
         <CardHeader>
           <CardTitle>Sign up</CardTitle>
           <CardDescription>
-            Don't miss out on all of the Betterment Book Club benefits.
+            Join the growing community of people gorwing together with the power
+            of reading and discussion!
           </CardDescription>
         </CardHeader>
-        <CardContent></CardContent>
-        <CardFooter className={cn("flex gap-4")}>
-          <Button className={cn("flex-1")}>Sign Up</Button>
-          <Button variant="outline" className={cn("flex-1")}>
-            Sign In
-          </Button>
-        </CardFooter>
+
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <CardContent className={cn("flex flex-col gap-2")}>
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>{"Name (optional)"}</FormLabel>
+                    <FormControl>
+                      <Input placeholder="John Smith" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Username</FormLabel>
+                    <FormControl>
+                      <Input placeholder="JSmith96" {...field} />
+                    </FormControl>
+                    <FormDescription>Your display name</FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Email" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Password" {...field} />
+                    </FormControl>
+                    <FormDescription>
+                      Password must be between 8 to 16 characters
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="confirmPassword"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Confirm password" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </CardContent>
+            <CardFooter className={cn("flex gap-4")}>
+              <Button type="submit" className={cn("flex-1")}>
+                Sign Up
+              </Button>
+              <Button variant="outline" className={cn("flex-1")}>
+                Sign In
+              </Button>
+            </CardFooter>
+          </form>
+        </Form>
       </Card>
     </div>
   );
