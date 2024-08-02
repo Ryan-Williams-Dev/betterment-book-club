70767deb47eccbfeda25c82f567a97248204d027
Ryan Williams <79440234+Ryan-Williams-Dev@users.noreply.github.com>
2024-08-02 03:01:46 +0000

    fixed icon color issue

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -7,7 +7,7 @@ const DashboardPage = () => {
     <div className="flex flex-1 flex-col p-8 gap-6">
       <TypographyH1>Welcome back, Ryan 👋</TypographyH1>
       <div className="flex flex-wrap gap-4">
-        <Card className="flex-1 min-w-[200px] dark:hover:bg-zinc-900 hover:shadow-xl">
+        <Card className="flex-1 min-w-[200px] bg-asparagus dark:hover:bg-zinc-900 hover:shadow-xl">
           <CardHeader>
             <CardTitle>Last read</CardTitle>
           </CardHeader>
