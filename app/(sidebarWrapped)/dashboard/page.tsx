2f6f70650bbb918a4b791a9d1e237b6cafae178d
Zenophage <79440234+Zenophage@users.noreply.github.com>
2024-09-02 20:16:04 -0700

    began work on google books api integration

--- app/(sidebarWrapped)/dashboard/page.tsx
+++ app/(sidebarWrapped)/dashboard/page.tsx
@@ -1,18 +1,22 @@
-import { TypographyH1 } from "@/components/typography";
+import { TypographyH1, TypographyH2,} from "@/components/typography";
 import { Button } from "@/components/ui/button";
-import { Card, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 
 const cardStyling = "flex-1 min-w-[200px] dark:hover:bg-zinc-900 hover:shadow-md"
 
 const DashboardPage = () => {
   return (
     <div className="flex flex-1 flex-col p-8 gap-6">
-      <TypographyH1>Welcome back, Ryan 👋</TypographyH1>
+      <TypographyH1>Dashboard</TypographyH1>
+      <TypographyH2>Welcome back, Ryan 👋</TypographyH2>
       <div className="flex flex-wrap gap-4">
         <Card className={cardStyling}>
           <CardHeader>
             <CardTitle>Last read</CardTitle>
           </CardHeader>
+          <CardContent>
+            HEllo
+          </CardContent>
           <CardFooter>
             <Button>Log reading</Button>
           </CardFooter>
